5f515c8c1292605b5f64bad367cfd3caa6d1cc8f
Ammar Askar <ammar@ammaraskar.com>
2024-07-26 16:23:17 -0400

    Throw exception when plate needs OCR

--- plate_analyzer/__init__.py
+++ plate_analyzer/__init__.py
@@ -3,12 +3,28 @@ from . import segmentation, text_extraction
 import pymupdf
 
 
+class PlateAnalyzerException(Exception):
+    """
+    Thrown when analysis of a plate fails
+    """
+    pass
+
+
+class PlateNeedsOCRException(PlateAnalyzerException):
+    pass
+
+
 def extract_information_from_plate(plate_path, debug=False):
     pdf = pymupdf.open(plate_path, filetype='pdf')
     plate = pdf[0]
 
     drawings = plate.get_drawings()
     textpage = plate.get_textpage()
+
+    # See if we need to run OCR on the page.
+    text = textpage.extractText()
+    if 'CATEGORY' not in text:
+        raise PlateNeedsOCRException("Plate requires OCR, no CATEGORY text")
 
     rectangles = segmentation.segment_plate_into_rectangles(
         plate, drawings, debug=debug
@@ -17,6 +33,9 @@ def extract_information_from_plate(plate_path, debug=False):
         plate, drawings, textpage, rectangles, debug=debug
     )
 
+    print("Has ARC:", text_info.has_dme_arc)
+    print("Has procedure turn:", text_info.has_procedure_turn)
+    print("Has hold-in-lieu:", text_info.has_hold_in_lieu_of_procedure_turn)
     for appch in text_info.approach_minimums:
         print(appch)
 
--- plate_analyzer/scrape_faa_dtpp_zip.py
+++ plate_analyzer/scrape_faa_dtpp_zip.py
@@ -6,22 +6,30 @@ zip file and analyzes each plate present in the zip.
 import zipfile
 import tempfile
 import pathlib
-from plate_analyzer import extract_information_from_plate
+from plate_analyzer import extract_information_from_plate, PlateNeedsOCRException
 
 
 def scan_dtpp_file(zip):
     with zipfile.ZipFile(zip, 'r') as dtpp_zip:
-        for file_info in dtpp_zip.infolist():
+        for i, file_info in enumerate(dtpp_zip.infolist()):
+            #if i not in (5,):
+            #    continue
+            if i > 100:
+                break
+
             print(file_info)
             with dtpp_zip.open(file_info.filename) as approach_zip:
-                analyze_zip_approach_file(approach_zip)
-            break
+                try:
+                    analyze_zip_approach_file(approach_zip)
+                except PlateNeedsOCRException:
+                    print("OCR needed")
+
 
 
 def analyze_zip_approach_file(file):
     temp_file = pathlib.Path(tempfile.gettempdir()) / 'analyze_plate.pdf'
     with temp_file.open('wb') as f:
         f.write(file.read())
-    extract_information_from_plate(temp_file)
+    extract_information_from_plate(temp_file, debug=True)
 
 
