a512f43800b4cdbfeaa3d5fa8cc06934b074d728
Ammar Askar <ammar@ammaraskar.com>
2024-07-27 14:29:07 -0400

    Fix quad -> lines by rounding before sorting

--- plate_analyzer/segmentation.py
+++ plate_analyzer/segmentation.py
@@ -20,14 +20,16 @@ def line_segment_as_rect_from_points(point1, point2):
 def make_rectangle_from_quad(quad: pymupdf.Quad) -> Optional[pymupdf.Rect]:
     # Sort by y-then-x to get the upper left, upper right and then lower left
     # lower right points of the quad.
-    quad_points_sorted = sorted([quad.ul, quad.ur, quad.ll, quad.lr],
-                                key=lambda p: (p.y, p.x))
+    points = [quad.ul, quad.ur, quad.ll, quad.lr]
+    quad_points = [pymupdf.Point(round(p.x, 0), round(p.y, 0)) for p in points]
+    quad_points_sorted = sorted(quad_points, key=lambda p: (p.y, p.x))
     ul, ur, ll, lr = quad_points_sorted
     # Check the top and bottom lines and see if they're straight.
-    if round(ul.y, 0) != round(ur.y, 0) or round(ll.y, 0) != round(lr.y, 0):
+    if ul.y != ur.y or ll.y != lr.y:
         return None
     # Check the left and right lines and see if they're straight.
-    if round(ul.x, 0) != round(ll.x, 0) or round(ur.x, 0) != round(lr.x, 0):
+    if ul.x != ll.x or ur.x != lr.x:
+        print(ul, ur, ll, lr)
         return None
     return quad.rect
 
