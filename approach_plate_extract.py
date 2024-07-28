f552cd36499648acab78bd7167cd4b7cb4dba814
Ammar Askar <ammar@ammaraskar.com>
2024-07-27 20:14:17 -0400

    Add testing function to verify contents of dtpp metadata

--- approach_plate_extract.py
+++ approach_plate_extract.py
@@ -1,8 +1,9 @@
 from plate_analyzer import extract_information_from_plate
-from plate_analyzer.scrape_faa_dtpp_zip import scan_dtpp_file
+from plate_analyzer.scrape_faa_dtpp_zip import scan_dtpp_file, verify_contents_of_zip_against_metadata
 
 
 # extract_information_from_plate("../../Downloads/03214R5.PDF", debug=True)
 # extract_information_from_plate("test_data/05222VT15.PDF", debug=True)
 
-scan_dtpp_file("../../Downloads/faa_dttp/DDTPPC_240711.zip")
+#scan_dtpp_file("../../Downloads/faa_dttp/DDTPPC_240711.zip")
+verify_contents_of_zip_against_metadata("../../Downloads/faa_dttp/")
--- plate_analyzer/scrape_faa_dtpp_zip.py
+++ plate_analyzer/scrape_faa_dtpp_zip.py
@@ -7,6 +7,7 @@ zip file and analyzes each plate present in the zip.
 import zipfile
 import tempfile
 import pathlib
+import xml.etree.ElementTree as ET
 from plate_analyzer import extract_information_from_plate, PlateNeedsOCRException
 
 
@@ -60,3 +61,54 @@ def analyze_zip_approach_file(file):
     with temp_file.open("wb") as f:
         f.write(file.read())
     extract_information_from_plate(temp_file, debug=True)
+
+
+def verify_contents_of_zip_against_metadata(folder):
+    """Checks the contents of the d-tpp_Metafile.xml against the zip files
+    containing the pdfs to see if all the expected files are present.
+
+    This is mostly just a testing function I wrote to see the contents of these
+    files myself, not used in any actual scraping code.
+    """
+    folder_path = pathlib.Path(folder)
+
+    approach_pdfs_in_metadata = set()
+    all_pdfs_in_metadata = set()
+    approach_pdfs_in_zips = set()
+
+    meta_file = folder_path / 'd-tpp_Metafile.xml'
+    with meta_file.open('r') as f:
+        metadata = ET.fromstring(f.read())
+
+    for airport in metadata.iter('airport_name'):
+        # Some local US only airports don't have icao identifiers.
+        airport_id = airport.attrib['icao_ident']
+        if not airport_id:
+            airport_id = airport.attrib['apt_ident']
+        
+        for record in airport.iter('record'):
+            pdf_file = record.find('pdf_name').text
+            if pdf_file:
+                all_pdfs_in_metadata.add(pdf_file)
+
+            # Specifically note instrument approaches.
+            chart_code = record.find('chart_code').text
+            if chart_code != 'IAP':
+                continue
+            approach_pdfs_in_metadata.add(pdf_file)
+
+    # Now collect the pdfs in the actual zips.
+    for zip_path in folder_path.glob('DDTPP*.zip'):
+        with zipfile.ZipFile(zip_path, "r") as dtpp_zip:
+            for file_info in dtpp_zip.infolist():
+                if 'compare_pdf' in file_info.filename or '.xml' in file_info.filename:
+                    break
+                approach_pdfs_in_zips.add(file_info.filename)
+
+    # Let's see if there's any metadata files not present in the zips.
+    in_metadata_not_in_zips = approach_pdfs_in_metadata - approach_pdfs_in_zips
+    assert in_metadata_not_in_zips == {'DELETED_JOB.PDF'}
+
+    # Let's look at the ones in the zip but not in the metadata.
+    in_zip_but_not_metadata = approach_pdfs_in_zips - all_pdfs_in_metadata
+    print("In zip but not metadata", in_zip_but_not_metadata)
