c5cf876156f843762cc4534393403652395ceaa2
t2691 <64107571+t2691@users.noreply.github.com>
2025-03-14 13:48:02 -0400

    Update approach_plate_extract.py
    change location of zip and FAACIFP18 to working directory of github actions runner
    change output.json to approaches.json

--- approach_plate_extract.py
+++ approach_plate_extract.py
@@ -8,9 +8,6 @@ if __name__ == "__main__":
 
     # cifp_analysis.analyze_cifp_file("../../Downloads/faa_dttp/FAACIFP18")
 
-    results = scrape_faa_dtpp_zip.analyze_dtpp_zips(
-        "../../Downloads/faa_dttp/250320",
-        cifp_file="../../Downloads/faa_dttp/250320/FAACIFP18",
-    )
-    with open("output.json", "w") as f:
+    results = scrape_faa_dtpp_zip.analyze_dtpp_zips("./", cifp_file="./FAACIFP18")
+    with open("approaches.json", "w") as f:
         f.write(results.model_dump_json())
